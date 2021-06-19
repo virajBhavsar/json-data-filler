09d53147a6f174fd82e039ecf5df82526b9a49cb
Viraj bhavsar <61341972+virajBhavsar@users.noreply.github.com>
2021-06-19 19:17:55 +0530

    Update refine.py

--- refine.py
+++ refine.py
@@ -29,13 +29,3 @@ class Merger:
             print("sorry, but schema and data is not in propper format")
         return a
     
-
-m = Merger()
-datafile = m.getFiles('schema.json','data.json')
-data = m.refine(datafile[0],datafile[1])
-
-y = json.dumps(data)
-with open('output.json','w') as f:
-    f.write(y)
-
-f.close()
